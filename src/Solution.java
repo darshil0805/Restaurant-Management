67d7bbb5c9fdce77c0c37910829873e105fbd8e3
darshil0805 <darshilfcbayern@gmail.com>
2021-11-17 09:45:50 +0530

    Commiting changes

--- src/Solution.java
+++ src/Solution.java
@@ -1,4 +1,8 @@
+import jdk.internal.access.JavaIOFileDescriptorAccess;
+
 import java.util.ArrayList;
+import java.util.Scanner;
+
 class Item {
     String name;
     int id;
@@ -9,71 +13,166 @@ class Item {
         this.name = name;
         this.price = price;
     }
+}
+
+
+class ItemQ extends Item {
+    int quantity;
+    float amount;
+
+    ItemQ(Item item, int quantity) {
+        super(item.id, item.name, item.price);
+        this.quantity = quantity;
+        this.amount = (this.quantity) * (this.price);
+    }
+}
+
+class Order extends Solution.CookingState {
+    ArrayList<ItemQ> currentOrder;
+
+    Order() {
+        currentOrder = new ArrayList<>();
+        Solution.Admin.allOrders.add(this);
+    }
+
+    float grossAmt() {
+        float amt = 0;
+        for (int i = 0; i < currentOrder.size(); i++) {
+            amt += currentOrder.get(i).amount;
+        }
+        return amt;
+    }
+
+    float netAmt() {
+        return grossAmt() * 1.18f;
+    }
+
+    public String toString() {
+        return super.toString();
+    }
+}
+
+class Customer {
+    String name;
+    private int id;
+    Order newOrder;
+
+
+    Customer(String name) {
+        this.name = name;
+    }
+
+    void getBill() {
+        System.out.println(newOrder.currentOrder.toString());
+        System.out.println("Gross amount for your order is:" + newOrder.grossAmt());
+        System.out.println("Net amount after 18% gst is: " + newOrder.netAmt());
+
+    }
+
+    String getMenu() {
+        return Solution.PriceList.priceList.toString();
+    }
+
+    void order() {
+        System.out.println("The order should be in the format : Item1id Item1quantity <enter>Item2id Item2quantity <enter> Item3id Item3quantity");
+        System.out.println("Press anything except an integer to end the order");
+        newOrder = new Order();
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextInt()) {
+            int id = scanner.nextInt();
+            int quant = scanner.nextInt();
+            ItemQ food = new ItemQ(Solution.PriceList.priceList.get(id), quant);
+            newOrder.currentOrder.add(food);
+        }
+    }
+
+    void getOrderStatus() {
+        System.out.println(newOrder);
+    }
 
 
 }
 
+
 public class Solution {
-    static final private class Admin {
+    static class PriceList {
+        static ArrayList<Item> priceList = new ArrayList<>();
+    }
+
+    static final class Admin {
+        static ArrayList<Order> allOrders = new ArrayList<>();
+        private float expenses = 0;
+
+        void changeItem(String name, int index, float price) {
+            Item item = new Item(index, name, price);
+            PriceList.priceList.set(index, item);
+        }
 
 
-        class PriceList {
-            //            Item I1= new Item();
-            ArrayList<Item> items = new ArrayList<>();
+        void addItem(String name, float price) {
+            int index = PriceList.priceList.size();
+            Item item = new Item(index, name, price);
+        }
 
-            void changeItem(String name, int index, float price) {
-                Item item = new Item(index, name, price);
-                items.set(index, item);
+        void removeItem(int index) {
+            PriceList.priceList.remove(index);
+            for (int i = 0; i < PriceList.priceList.size(); i++) {
+                Item item_change = PriceList.priceList.get(i);
+                item_change.id = i;
+                PriceList.priceList.set(i, item_change);
             }
+        }
 
-
-            void addItem(String name, float price) {
-                int index = items.size();
-                Item item = new Item(index, name, price);
+        float revenue() {
+            float rev = 0;
+            for (int i = 0; i < allOrders.size(); i++) {
+                rev += allOrders.get(i).grossAmt();
             }
+            return rev;
+        }
 
-            void removeItem(int index) {
-                items.remove(index);
-                for (int i = 0; i < items.size(); i++) {
-                    Item item_change = items.get(i);
-                    item_change.id = i;
-                    items.set(i, item_change);
-                }
-            }
+        float profit() {
+            return this.revenue() - expenses;
+        }
+
+        void addExpense(float amt) {
+            expenses += amt;
         }
     }
 
-    static class RestaurantState{
+    static class RestaurantState {
         private int state = 0;
+
         @Override
-        public String toString(){
+        public String toString() {
             // 0 stands for closed/full 1 stands for open
 
-            if(state ==0){
+            if (state == 0) {
                 return "Restaurant is Closed/Full";
             }
             return "Restaurant is Open";
         }
 
-        void setState(int state){
+        void setState(int state) {
             this.state = state;
         }
     }
 
-    static class CookingState{
-        private int state = 0;
+    static class CookingState {
+        private int cState = 0;
+
         @Override
-        public String toString(){
+        public String toString() {
             // 0 stands for closed/full 1 stands for open
 
-            if(state ==0){
+            if (cState == 0) {
                 return "Restaurant is Closed/Full";
             }
             return "Restaurant is Open";
         }
 
-        void setState(int state){
-            this.state = state;
+        void setState(int state) {
+            this.cState = state;
         }
     }
 
@@ -81,7 +180,6 @@ public class Solution {
     public static void main(String[] args) {
         Admin a1 = new Admin();
     }
-
 
 
 }
